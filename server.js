e359acd661a02e4805ec83db8e576d68fc6bf31b
Travis Herrick <quasimonomial@gmail.com>
2018-06-11 17:25:54 -0700

    stop breaking bug

--- server.js
+++ server.js
@@ -31,10 +31,10 @@ router.get('/weather', function(req, res, next) {
 
 // Use HTTP instead of HTTPS
 app.use((req, res, next) => {
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next()
   }
 })
 
